18be0d9fbe2cfd516dddcb728a2b189ab3162b9e
Chetanbodapatla <chetan.bodapatla@mobigesture.com>
2022-04-06 18:44:27 +0530

    reduce the execution time of code, changed main function and other parts to optimize the code

--- Intersection.py
+++ Intersection.py
@@ -1,74 +1,48 @@
 import re
 import time
 import sys
-def checking_special_chr_space(var):
-    for y in var:
-        if len(re.findall("[0-9@a-zA-Z._-]", y)) == 0:
+
+
+def checking_email_except_repetitive_specialchar(string):
+
+    pattern = '^[a-zA-Z0-9][-a-zA-Z0-9._]*[a-zA-Z0-9]@[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]\.[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]$'
+    pattern2 = '^[a-zA-Z0-9]([-a-zA-Z0-9._][a-zA-Z0-9])*@[a-zA-Z0-9]([a-zA-Z0-9-][a-zA-Z0-9])*\.[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]$'
+    if re.match(pattern, string) or re.match(pattern2, string):
+        return True
+    else:
+        return False
+
+def checking_consecutive_specialchar(char):
+    for y in range(len(char)-1):
+        if  char[y] in ".-_" and  char[y+1] in ".-_":
             return False
     return True
-
-
-def checking_first_last_digit(var):
-    if len(re.findall("[0-9a-zA-Z]", var[0])) ==0 or len(re.findall("[0-9a-zA-Z]", var[-1])) ==0:
-        return False
-    else:
-        return True
-
-def checking_gmailsymbols(var):
-    if "." in var and "@" in var and var.count("@")==1:
-        return True
-    else:
-        return False
-
-def checking_char_around_specialchar(var):
-    if  var[var.rfind("@")-1] in "._-" or var[var.rfind("@")+1] in "._-" :
-        return False
-    elif var[var.rfind(".") - 1] in "._-" or var[var.rfind(".") + 1] in "._-":
-        return False
-    else:
-        return True
-
-def checking_consecutive_specialchar(var):
-    for y in range(len(var)-1):
-        if  var[y] in ".-_" and  var[y+1] in ".-_":
-            return False
-    return True
-
 
 def checking_email(var):
-    if checking_special_chr_space(var) and checking_first_last_digit(var):
-        if checking_gmailsymbols(var) and checking_char_around_specialchar(var):
-            if checking_consecutive_specialchar(var):
-                return True
-    return False
-
-def adding_email_to_hashfunction(email,emails):
-    if email.upper() in emails.values():
-        return False
+    if checking_email_except_repetitive_specialchar(var):
+        if checking_consecutive_specialchar(var):
+            return True
     else:
-        return True
+        return False
 
 def main_function(y):
     with open(y, 'r') as f:
         emails = {}
-        i = 0
         for email in f.readlines():
             if checking_email(email.strip()):
-                if adding_email_to_hashfunction(email, emails):
-                    emails[i] = email.upper()
-                    i += 1
+                emails[email.upper()] = 1
     return emails
 
 def intersection_function(list1,list2,y):
-    L=[]
-    for value in list1.values():
-        if value in list2.values():
-            L.append(value)
+    list = []
+    for key in list1.keys():
+        if key in list2:
+            list.append(key)
 
     with open(y, "w") as f:
-        for i in L:
-            f.write(i + "\n")
-    return L
+        for value in list:
+            f.write(value + "\n")
+    return list
 
 
 # main function
